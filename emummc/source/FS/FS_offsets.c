f55b989070b5c1894d2334ebd63eacc4444dc85a
nunzawa <loijgtnhfv@gmail.com>
2026-09-11 23:12:39 +0900

    Revert "emummc: update for 23.0.0"
    This reverts commit bce987792d0b521d46af2d2acd01b0727905876e.

--- emummc/source/FS/FS_offsets.c
+++ emummc/source/FS/FS_offsets.c
@@ -87,8 +87,6 @@
 #include "offsets/2200_exfat.h"
 #include "offsets/2250.h"
 #include "offsets/2250_exfat.h"
-#include "offsets/2300.h"
-#include "offsets/2300_exfat.h"
 #include "../utils/fatal.h"
 
 #define GET_OFFSET_STRUCT_NAME(vers) g_offsets##vers
@@ -187,8 +185,6 @@ DEFINE_OFFSET_STRUCT(_2200);
 DEFINE_OFFSET_STRUCT(_2200_EXFAT);
 DEFINE_OFFSET_STRUCT(_2250);
 DEFINE_OFFSET_STRUCT(_2250_EXFAT);
-DEFINE_OFFSET_STRUCT(_2300);
-DEFINE_OFFSET_STRUCT(_2300_EXFAT);
 
 const fs_offsets_t *get_fs_offsets(enum FS_VER version) {
     switch (version) {
@@ -334,10 +330,6 @@ const fs_offsets_t *get_fs_offsets(enum FS_VER version) {
             return &(GET_OFFSET_STRUCT_NAME(_2250));
         case FS_VER_22_5_0_EXFAT:
             return &(GET_OFFSET_STRUCT_NAME(_2250_EXFAT));
-        case FS_VER_23_0_0:
-            return &(GET_OFFSET_STRUCT_NAME(_2300));
-        case FS_VER_23_0_0_EXFAT:
-            return &(GET_OFFSET_STRUCT_NAME(_2300_EXFAT));
         default:
             fatal_abort(Fatal_UnknownVersion);
     }
